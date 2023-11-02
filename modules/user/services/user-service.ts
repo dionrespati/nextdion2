c72ff246dd338d7fec64d7872ce619affb412ead
dionrespati82 <dionrespati@gmail.com>
2023-11-02 11:56:10 +0700

    setting env

--- modules/user/services/user-service.ts
+++ modules/user/services/user-service.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from "axios";
 import { IUserInfo } from "@types";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export async function getInfoUser(searchTerm: string): Promise<IUserInfo> {
   let paramSearch = "";
   if (searchTerm !== "") {
     paramSearch = `1`;
   }
   return axios
-    .get(`https://dummyjson.com/users/${paramSearch}`)
+    .get(`${apiUrl}/users/${paramSearch}`)
     .then((response: AxiosResponse<IUserInfo>) => response.data)
     .catch((error) => {
       throw new Error(error.response?.data?.message || "Failed to fetch user");
