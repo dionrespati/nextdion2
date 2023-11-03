478392d1f5eb838b2fe4bb68c8cb7afbc811c944
dionrespati82 <dionrespati@gmail.com>
2023-11-03 21:14:24 +0700

    mock api

---
+++ modules/products/constant.ts
@@ -0,0 +1,152 @@
+[
+  {
+    productName: "Man Cool Shirt",
+    productDesc:
+      "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+    productCategory: "Fashion Man",
+    price: "787.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [
+      {
+        size: "S",
+        price: "787.00",
+      },
+      {
+        size: "M",
+        price: "787.00",
+      },
+      {
+        size: "L",
+        price: "787.00",
+      },
+      {
+        size: "XL",
+        price: "787.00",
+      },
+      {
+        size: "XXL",
+        price: "789.00",
+      },
+      {
+        size: "XXXL",
+        price: "790.00",
+      },
+    ],
+    id: "1",
+  },
+  {
+    productName: "Gay Man",
+    productDesc:
+      "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    productCategory: "Fashion Man",
+    price: "585.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [
+      {
+        size: "S",
+        price: "585.00",
+      },
+      {
+        size: "M",
+        price: "585.00",
+      },
+      {
+        size: "L",
+        price: "585.00",
+      },
+      {
+        size: "XL",
+        price: "585.00",
+      },
+      {
+        size: "XXL",
+        price: "590.00",
+      },
+      {
+        size: "XXXL",
+        price: "590.00",
+      },
+    ],
+    id: "2",
+  },
+  {
+    productName: "Chips",
+    productDesc:
+      "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+    productCategory: "Handcrafted",
+    price: "889.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "3",
+  },
+  {
+    productName: "Pizza",
+    productDesc:
+      "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+    productCategory: "Recycled",
+    price: "267.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "4",
+  },
+  {
+    productName: "Tuna",
+    productDesc:
+      "The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality",
+    productCategory: "Practical",
+    price: "698.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "5",
+  },
+  {
+    productName: "Hat",
+    productDesc:
+      "The Apollotech B340 is an affordable wireless mouse with reliable connectivity, 12 months battery life and modern design",
+    productCategory: "Small",
+    price: "470.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "6",
+  },
+  {
+    productName: "Tuna",
+    productDesc:
+      "New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart",
+    productCategory: "Sleek",
+    price: "888.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "7",
+  },
+  {
+    productName: "Cheese",
+    productDesc:
+      "Carbonite web goalkeeper gloves are ergonomically designed to give easy fit",
+    productCategory: "Sleek",
+    price: "239.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "8",
+  },
+  {
+    productName: "Chicken",
+    productDesc:
+      "The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J",
+    productCategory: "Tasty",
+    price: "591.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "9",
+  },
+  {
+    productName: "Keyboard",
+    productDesc:
+      "Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support",
+    productCategory: "Generic",
+    price: "229.00",
+    image: "https://loremflickr.com/640/480/fashion",
+    varian: [],
+    id: "10",
+  },
+];
